1c2379cd57a6dacee260d6d737fecd921a2d7fc4
Husseinn-Essam <112550601+Husseinn-Essam@users.noreply.github.com>
2023-02-02 11:56:52 +0200

    2 player mode complete

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ const player = (mark) => {
 };
 
 const controlFlow = (function () {
-  //all win conditions
+
   const winConditions=[[0, 1, 2],
 	[3, 4, 5],
 	[6, 7, 8],
@@ -43,10 +43,12 @@ const controlFlow = (function () {
 	[2, 5, 8],
 	[0, 4, 8],
 	[2, 4, 6]];
+  const banner= document.querySelector(".banner");
+  const startGame = document.querySelector(".start");  
+  let endGame=false;
   
-
 // win condition
-  function win() {
+  function win(mark) {
       let win = false;
       for(let i=0;i<winConditions.length;i++){
         const winCond = winConditions[i];
@@ -59,38 +61,57 @@ const controlFlow = (function () {
         }
       }
       if(win ==true){
-        console.log("win");
+        if(mark=="O"){
+          banner.textContent="Player 1 Wins!";
+          endGame=true;
+        }
+        else{
+          banner.textContent="Player 2 Wins!";
+          endGame=true;
+        }
       }
     }
-  
-//game flow
+    // restarting the game
+  function gameOver(){
+    startGame.addEventListener('click',()=>{
+      for(var i = 0 ;i <9;i++){
+        gameBoard.board[i]="";
+        document.querySelectorAll(".spot")[i].textContent="";
+      }
+      banner.textContent="BATTLE";
+      endGame=false;
+    })
+  }
+// game flow
   function game() {
-    gameBoard.drawBoard();
     let currentPlayer = 1;
     const spots = document.querySelectorAll(".spot");
     const player1 = player("O");
     const player2 = player("X");
     for (var i = 0; i < 9; i++) {
       spots[i].addEventListener("click", (e) => {
-        if (currentPlayer == 1) {
-          console.log(player1.mark);
-          player1.play(e.target, player1.mark);
-          gameBoard.update(spots);
-          currentPlayer = 2;
-          win();
-        } else {
-          console.log(player2.mark);
-          player2.play(e.target, player2.mark);
-          gameBoard.update(spots);
-          //win(player2.mark);
-          currentPlayer = 1;
+        if (endGame==false){
+          if (currentPlayer == 1) {
+            player1.play(e.target, player1.mark);
+            gameBoard.update(spots);
+            win(player1.mark);
+            currentPlayer = 2;
+            
+          } else {
+            player2.play(e.target, player2.mark);
+            gameBoard.update(spots);
+            win(player2.mark);
+            currentPlayer = 1;
+          }
         }
       });
     }
   }
   return {
     game: game,
+    gameOver:gameOver,
   };
 })();
-
+gameBoard.drawBoard();
 controlFlow.game();
+controlFlow.gameOver();
